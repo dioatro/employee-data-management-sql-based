1662c2a0bad6d871a2eb3e6e7a8b7a58c4a4a51c
Dioatro <216211484+dioatro@users.noreply.github.com>
2025-11-09 15:17:25 +0530

    bug fixes

--- main.py
+++ main.py
@@ -29,23 +29,26 @@ def add_data(): # data=[empno,name,gender,age,role,dept,salary]
         dept=input("Enter Department: ")
         salary=int(input("Enter Emplyee's Salary: "))
         obj_cursor=mydb.cursor()
-        obj_cursor.execute('insert into employee values("{emp_no}","{name}","{gender}","{age}","{role}","{dept}","{salary}")')
+        obj_cursor.execute("insert into employee values({0},'{1}','{2}',{3},'{4}','{5}',{6})".format(emp_no,name,gender,age,role,dept,salary))
+        mydb.commit()
         ch=input("wanna enter more employee's data? (Y/N): ")
-        if ch.upper() == 'N' or ch.upper != 'Y': #just trying to be effecient ;)
-            break
+        if ch.upper() == 'N': #just trying to be effecient ;)
+            mydb.close()
+            menu()
 
 def remove_data():
     del_emp=int(input("Enter Employee No. to be removed: "))
-    try: #checking if that emp no. exists or not 
-        obj_cursor=mydb.cursor()
-        obj_cursor.execute('delete from employee where Emp_no = "{del_emp}"')
-    except:
-        print("Employee No. Not Found!")
+    obj_cursor = mydb.cursor()
+    obj_cursor.execute("DELETE FROM employee WHERE Emp_no = {0}".format(del_emp))
+    mydb.commit()
+    if obj_cursor.rowcount == 0:
+        print("Employee No. not found!")
+        menu()
+    else:
+        print("Employee removed successfully!")
+        menu()
 
-def config_data():
-    conf_emp=int(input("Enter the Employee No. to be configured: "))
-    try: #checking if that emp no. exists or not 
-        obj_cursor=mydb.cursor()
-        obj_cursor.execute("update")
-    except:
-        print("Employee No. Not Found!")
+#def config_data():
+    
+
+menu()
